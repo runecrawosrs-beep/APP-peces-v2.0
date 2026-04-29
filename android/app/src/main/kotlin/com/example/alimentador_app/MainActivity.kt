71343c786f087d77b965f38e234c24174a117a3e
Anyelver Torres <runecrawosrs@gmail.com>
2026-04-29 11:55:13 -0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/alimentador_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.alimentador_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ assets/README.txt
@@ -0,0 +1,2 @@
+Coloca aqui tu logo como: logo.png
+Ruta esperada por la app: assets/logo.png
